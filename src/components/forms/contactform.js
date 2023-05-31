bb390ecb20bc50476d8f329b56b353fc70de3eb2
Sami <coolsweetsami@gmail.com>
2023-05-31 17:58:08 -0400

    crated a contact form

---
+++ src/components/forms/contactform.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import '../forms/form.css';
+
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form validation
+    const validationErrors = {};
+
+    if (!formData.name) {
+      validationErrors.name = 'Please enter your name';
+    }
+
+    if (!formData.email) {
+      validationErrors.email = 'Please enter your email';
+    } else if (!isValidEmail(formData.email)) {
+      validationErrors.email = 'Please enter a valid email';
+    }
+
+    if (!formData.message) {
+      validationErrors.message = 'Please enter your message';
+    }
+
+    // Display validation errors, if any
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    // Clear any previous validation errors
+    setErrors({});
+
+    console.log(formData);
+  };
+
+  const isValidEmail = (email) => {
+    // Simple email validation using regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        ></textarea>
+        {errors.message && <span className="error">{errors.message}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default ContactForm;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from '../forms/contactform';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default Contact;
