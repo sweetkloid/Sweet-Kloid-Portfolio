acd97e8fb6558460abc5037b8dc63a2f07cfbb48
Sami <coolsweetsami@gmail.com>
2023-07-11 20:36:06 -0400

    fixes to resume download, updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,51 +1,49 @@
 import React from 'react';
+import resumeFile from '../../assets/SweetKloid.pdf';
 import '../../styles/Resume.css';
 
 export default function Resume() {
- 
-  const handleDownload = () => {
-    // Perform any necessary logic before the download
-    // For example, fetch the file URL or prepare the file data
-
-    // Create a temporary download link
-    const link = document.createElement('a');
-    link.href = '../../assets/FAKE.pdf'; // Replace with the actual file URL or data
-    link.download = 'FAKE.pdf'; // Replace with the desired file name and extension
-    document.body.appendChild(link);
-
-    // Trigger the click event to start the download
-    link.click();
-
-    // Clean up by removing the temporary link from the DOM
-    document.body.removeChild(link);
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(resumeFile);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'SweetKloid.pdf';
+      link.click();
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error('Error occurred while downloading the file:', error);
+    }
   };
 
- return(
-  <div class= "section3">
-    <h1 class="intro">Resume</h1>
-    <div class="downloadme">
-      <h3 className='types'>Download Resume:</h3>
-      <button onClick={handleDownload}>Download File</button>
+  return(
+    <div className= "section3">
+      <h1 className="intro">Resume</h1>
+      <div className="downloadme">
+        <h3 className='types'>Download Resume:</h3>
+        <button onClick={handleDownload}>Download File</button>
+      </div>
+    <h2 className='types2'>Front-end Proficiencies</h2>
+      <ul>HTML</ul>
+      <ul>CSS</ul>
+      <ul>JavaScript</ul>
+      <ul>JQuery</ul>
+      <ul>responsive design</ul>
+      <ul>React</ul>
+      <ul>Bootstrap</ul>
+      <ul>Bulma</ul>
+   
+    <h2 className='types2'>Back-end Proficiencies</h2>
+      <ul>APIs</ul>
+      <ul>Node</ul>
+      <ul>Express</ul>
+      <ul>MySQL Sequelize</ul>
+      <ul>MongoDB, Mongoose</ul>
+      <ul>REST</ul>
+      <ul>GraphQL</ul>
+    
     </div>
-  <h2 className='types2'>Front-end Proficiencies</h2>
-    <ul>HTML</ul>
-    <ul>CSS</ul>
-    <ul>JavaScript</ul>
-    <ul>JQuery</ul>
-    <ul>responsive design</ul>
-    <ul>React</ul>
-    <ul>Bootstrap</ul>
-    <ul>Bulma</ul>
- 
-  <h2 className='types2'>Back-end Proficiencies</h2>
-    <ul>APIs</ul>
-    <ul>Node</ul>
-    <ul>Express</ul>
-    <ul>MySQL Sequelize</ul>
-    <ul>MongoDB, Mongoose</ul>
-    <ul>REST</ul>
-    <ul>GraphQL</ul>
-  
-  </div>
-);
-}
+  );
+  }
